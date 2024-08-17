ab7663bf29cdab0ed3d89c953e83ad10f67ee53a
hiroto <rmoraisu@protonmail.com>
2024-08-17 17:46:00 -0300

    fixes #9

--- projeto/recursos/jogo/mobiliza.gd
+++ projeto/recursos/jogo/mobiliza.gd
@@ -4,12 +4,17 @@ export var tempo: int = 100
 export var total_seguidores: int = 0
 
 onready var _temporizador: Timer = $Temporizador
+onready var jogador = $YSort/jogador as Jogador
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Global.tempo_total = tempo
 	_temporizador.wait_time = tempo
 	_temporizador.autostart = false
+
+	jogador.barra_tempo.max_value = _temporizador.wait_time
+	jogador.barra_tempo.value = _temporizador.wait_time
+	
 	_temporizador.start()
 	for crianca in $YSort.get_children():
 		if 'pessoa' in crianca.name:
