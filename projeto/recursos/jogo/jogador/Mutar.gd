49427628a8515c826d2a97d2bfcd72ef030e76d5
hiroto <rmoraisu@protonmail.com>
2024-10-03 17:30:43 -0300

    mute button

---
+++ projeto/recursos/jogo/jogador/Mutar.gd
@@ -0,0 +1,24 @@
+extends TouchScreenButton
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+var mutado := false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
+
+
+func _on_Mutar_pressed() -> void:
+	# de https://forum.godotengine.org/t/mute-button-sounds-on-all-scenes/8964
+	mutado = !mutado
+	var bus_idx = AudioServer.get_bus_index("Master")
+	AudioServer.set_bus_mute(bus_idx, mutado)
+	$Sprite.visible = mutado
